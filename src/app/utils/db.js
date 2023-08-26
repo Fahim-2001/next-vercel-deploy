2ea2669fce357d4bdeaf75989bf7489d739be367
Fahim2001 <mdyasirofficial2001@gmail.com>
2023-08-26 22:57:47 +0600

    Second Commit

--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_CONNECT);
+    mongoose.set('strictQuery',true)
     // mongoose.set('strictQuery',false);
   } catch (error) {
     throw new Error("Connection Failed!");
