731928b99a5e18cf1b03c07b136bd9f255edd1a0
Fahim2001 <mdyasirofficial2001@gmail.com>
2023-11-28 22:37:23 +0600

    API redesigned

--- src/app/components/AddUser/AddUser.jsx
+++ src/app/components/AddUser/AddUser.jsx
@@ -18,7 +18,7 @@ const AddUser = () => {
             joiningDate: e.target.joining_date.value,
         }
 
-        const req = await fetch('next-practise-delta.vercel.app/api/users', {
+        const req = await fetch('https://next-practise-delta.vercel.app/api/users', {
             method:"POST",
             headers:{
                 'Content-Type':'application/json'
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,7 +5,7 @@ import AddUser from "../components/AddUser/AddUser";
 
 const Dashboard = async() => {
 
-  const data = await fetch('next-practise-delta.vercel.app/api/users', {cache:'no-store'}).then(res=>res.json())
+  const data = await fetch('https://next-practise-delta.vercel.app/api/users', {cache:'no-store'}).then(res=>res.json())
 
   // console.log(data);
 
