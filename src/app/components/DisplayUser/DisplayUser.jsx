e3598fc762c70742672c76d2c374c17e4e800eab
Fahim2001 <mdyasirofficial2001@gmail.com>
2023-11-29 15:52:11 +0600

    Fetches activated

--- src/app/components/DisplayUser/DisplayUser.jsx
+++ src/app/components/DisplayUser/DisplayUser.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const DisplayUser = async () => {
-    // const data = await fetch(process.env.NEXT_PUBLIC_BASE_URL+"api/users", {
-    //     cache: "no-store",
-    //   }).then((res) => res.json());
+    const data = await fetch(process.env.NEXT_PUBLIC_BASE_URL+"api/users", {
+        cache: "no-store",
+      }).then((res) => res.json());
     // console.log(data)
   return (
     <div>
-      {/* {data.map((user) => (
+      {data.map((user) => (
         <p key={user.emp_id}>{user.emp_fname}</p>
-      ))} */}
+      ))}
     </div>
   );
 };
